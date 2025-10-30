be8b10d34bd106fbca6aad1f7502ee3f6cb31515
ggalas-cell <ggalas@gaming.tech>
2025-10-30 13:11:06 +0100

    feat : display

--- POO/Fight console/character.cpp
+++ POO/Fight console/character.cpp
@@ -7,4 +7,10 @@ character::character(const std::string& n, int d, int ch,int mh)
 //void takedmg(int Hp,int dmg)
 //{
 //	Hp -= dmg;
-//}
+//}a
+void display()const
+{
+    std::cout<<name(n)<<std::endl;
+    std::cout<<"Damage :"<<dmg(d)<<std::endl;
+    std::cout<<"Hp :"<<currentHp(ch)<<"/"<<maxHp(mh)<<std::endl;
+}
