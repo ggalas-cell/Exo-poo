32475101382ab3e48da39242b0e3d1b31bb52cd9
ggalas-cell <ggalas@gaming.tech>
2025-10-30 13:12:17 +0100

    fix : override

--- POO/Fight console/wizard.h
+++ POO/Fight console/wizard.h
@@ -1,9 +1,11 @@
+
 #pragma once
 #include "character.h"
 class wizard : public character
 {
 public:
-	void attack()
+// heal
+	void attack() override
 	{
 		std::cout << "Attaque du Wizard" << std::endl;
 	}
