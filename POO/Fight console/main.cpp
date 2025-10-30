924f0644a73579a0c374ba6c819cbf931049a3ad
ggalas-cell <ggalas@gaming.tech>
2025-10-30 13:08:16 +0100

    feat : display

--- POO/Fight console/main.cpp
+++ POO/Fight console/main.cpp
@@ -8,5 +8,11 @@ int main()
 {
 	character warrior("Borg", 20, 500, 500);
 	character wizard ("The fool", 30, 300, 300);
+	void  
 	character maximegoblin("Maxime Goblin", 999,100,100);
+	
+// 	void display()const
+// polymorphisme vector avec character
+//     définir dans main attaque wizard warrior et mg
+	return 0;
 }
