39b0ec92a4e28b11a867a9d2e7aaed99706d58af
ggalas-cell <ggalas@gaming.tech>
2025-10-31 17:29:43 +0100

    feat : game stat

--- POO/Fight console/game.cpp
+++ POO/Fight console/game.cpp
@@ -16,6 +16,9 @@ void game::loop(player* player)
 	{
 		std::cout << "Tour " << turn++ << " : voulez vous attaquez (a) ou fuir (f) ?";
 		std::cin >> player->input;
+		
+		
+
 		switch (player->input)
 		{
 		case 'a':
@@ -25,6 +28,7 @@ void game::loop(player* player)
 			std::cout << "Vous fuyez" << std::endl;
 			isrunning = false;
 			break;
+
 		default:
 			std::cout << "commande inconnue" << std::endl;
 			break;
--- POO/Fight console/game.h
+++ POO/Fight console/game.h
@@ -1,10 +1,19 @@
 #pragma once
 #include "player.h"
+enum class gamestat
+{
+	play,
+	death,
+	win,
+	loose,
+	quit
+};
 class game
 {
 private:
 	int turn;
 	bool isrunning;
+	gamestat stat;
 public:
 	game(): isrunning(false),turn(1){}
 	void initialise();
