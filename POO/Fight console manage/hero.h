ec5b6365f3cbbd626db6118e913e3991e653a3a2
ggalas-cell <ggalas@gaming.tech>
2025-10-31 13:12:08 +0100

    feat : dmg

--- POO/Fight console manage/hero.h
+++ POO/Fight console manage/hero.h
@@ -1,15 +1,17 @@
+
 #pragma once
 #include <string>
 #include <iostream>
 class hero
 {
 private:
+	std::string name;
 	int currentHp;
 	int maxHp;
-	std::string name;
+	int dmg
 public:
-
-
-
+    hero(const std::st  ring n, int ch, int mh, int d)
+    // void attack() = d;
+    virtual ~hero(){}
+    // int death(int ch);
 };
-
